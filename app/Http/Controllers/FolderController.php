2de13864fef00d62540264682509ffdf6325a0e1
YukakoNakayama <sweetsky2089@gmail.com>
2020-09-27 22:48:41 +0900

    first commit

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use App\Http\Requests\CreateFolder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+
+    // 引数にインポートしたRequestクラスを受け入れる
+    public function create(CreateFolder $request)
+    {
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+        // インスタンスの状態をユーザーに紐づけて保存
+        Auth::user()->folders()->save($folder);
+
+        return redirect()->route('tasks.index', [
+            'folder' => $folder->id,
+        ]);
+    }
+}
